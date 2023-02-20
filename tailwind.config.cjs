6a6a12bac6771e447a9a2c44affe5cdda231941e
GeorgeKryptonian <georgekryptonian@gmail.com>
2023-02-20 01:13:12 +0100

    Add custom colors.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,12 @@ module.exports = {
         gilroy: "'Gilroy'",
         galien: "'Galien'",
       },
+      colors: {
+        'midnight-blue': "#1F3F68",
+        'royal-blue': "#316099",
+        'water-blue': "#356EAD",
+        'cornflower-blue': "#5A98D0",
+      },
     },
   },
   plugins: [],
