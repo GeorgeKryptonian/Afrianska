e16fefe687c95318924e18eaf37fbd5e77c2b526
GeorgeKryptonian <georgekryptonian@gmail.com>
2023-02-20 01:39:03 +0100

    Add new breakpoints and make minor updates.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,14 +7,20 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        gilroy: "'Gilroy'",
-        galien: "'Galien'",
+        'gilroy': "'Gilroy'",
+        'galien': "'Galien'",
       },
       colors: {
         'midnight-blue': "#1F3F68",
         'royal-blue': "#316099",
         'water-blue': "#356EAD",
         'cornflower-blue': "#5A98D0",
+      },
+      screens: {
+        'xs': "320px",
+        'sm': "480px",
+        'xl': "1366px",
+        '2xl': "1600px",
       },
     },
   },
