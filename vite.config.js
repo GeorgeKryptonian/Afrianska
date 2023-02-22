82bef30535a07c8ccff5a5f5896d1b40afb3a1be
GeorgeKryptonian <georgekryptonian@gmail.com>
2023-02-22 14:43:54 +0100

    Add aliases.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/Afrianska/",
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, 'src'),
+      '@images': path.resolve(__dirname, 'src/assets/images'),
+    },
+  },
 })
