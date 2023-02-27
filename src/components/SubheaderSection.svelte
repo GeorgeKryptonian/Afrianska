3c089dcd847cf3a1f2745f1a0a189211ee429e91
GeorgeKryptonian <georgekryptonian@gmail.com>
2023-02-27 12:23:42 +0100

    Add "SubheaderSection.svelte".

---
+++ src/components/SubheaderSection.svelte
@@ -0,0 +1,10 @@
+<script>
+    import subheaderBackground from '@images/vectors/subheadingBackground.svg'
+    export let heading;
+    export let description;
+</script>
+
+<section class="bg-cover bg-center bg-no-repeat p-6 text-center text-white space-y-4 md:px-20 lg:px-40 xl:px-80 xl:py-12" style="background-image: url({subheaderBackground})">
+    <h1 class="font-semibold text-[44px]">{heading}</h1>
+    <p class="text-xl opacity-40">{description}</p>
+</section>
