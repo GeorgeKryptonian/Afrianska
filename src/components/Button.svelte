3def4b9efbcd2cb03ce34de902d8feafa1279151
GeorgeKryptonian <georgekryptonian@gmail.com>
2023-02-28 15:19:01 +0100

    Add "Button.svelte" and make minor updates ("Footer.svelte", "SubheaderSection.svelte").

---
+++ src/components/Button.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let
+        text = 'Default Text',
+        sizeClasses = 'h-14 w-56',
+        additionalClasses = '';
+</script>
+
+<button class="{additionalClasses} {sizeClasses} rounded-xl bg-cornflower-blue">{text}</button>
