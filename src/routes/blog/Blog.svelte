43d04d6939a652cbd69e13bef6bda6318d1f4285
GeorgeKryptonian <georgekryptonian@gmail.com>
2023-03-24 13:20:55 +0100

    Combine components (shared, sub) in "Blog.svelte".

--- src/routes/blog/Blog.svelte
+++ src/routes/blog/Blog.svelte
@@ -3,9 +3,19 @@
 </svelte:head>
 
 <script>
-
+    import Header from '@src/components/Header.svelte';
+    import SubheaderSection from '@src/components/SubheaderSection.svelte';
+    import SearchArticleSection from '@src/routes/blog/components/SearchArticleSection.svelte';
+    import PopularArticlesSection from '@src/routes/blog/components/PopularArticlesSection.svelte';
+    import AllArticlesSection from '@src/routes/blog/components/AllArticlesSection.svelte';
+    import Footer from '@src/components/Footer.svelte';
 </script>
 
+<Header />
 <main>
-
+    <SubheaderSection heading="Blog Afrianska" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." additionalSectionClasses="mb-14" />
+    <SearchArticleSection additionalSectionClasses="mb-14" />
+    <PopularArticlesSection additionalSectionClasses="mb-14" />
+    <AllArticlesSection additionalSectionClasses="mb-24" />
 </main>
+<Footer />
