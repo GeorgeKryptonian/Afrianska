3011d03756c22156018f0c7ee83eac18abc7a5df
GeorgeKryptonian <georgekryptonian@gmail.com>
2023-04-07 17:55:37 +0200

    Add "MapSection.svelte".

---
+++ src/routes/contact/components/MapSection.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let additionalSectionClasses = '';
+</script>
+
+<section class="{additionalSectionClasses} px-6 md:px-24 xl:px-52">
+    <iframe class="w-full h-[380px] xl:h-[480px] 2xl:h-[580px]" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6152.446456733501!2d-82.99409526511232!3d39.55457390000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x884776d27a90183f%3A0x44182505d256eeb2!2z0J_RgNGD0LQg0KXQuNGC0LvQtdGA!5e0!3m2!1sru!2spl!4v1680881585875!5m2!1sru!2spl" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</section>
